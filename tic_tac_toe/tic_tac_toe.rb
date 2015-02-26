e8d7b8915ae732cb69146b702935b1c7b22c710a
Alex Hardt <hardt.alex@gmail.com>
2015-02-26 08:20:25 -0600

    building tests for tic tac toe

---
+++ tic_tac_toe/tic_tac_toe.rb
@@ -0,0 +1,15 @@
+class Cell
+  attr_accessor :value
+
+  def initialize(value="")
+    @value = value
+  end
+end
+
+class Player
+  attr_reader :color, :name
+  def initialize(input)
+    @color = input.fetch(:color)
+    @name = input.fetch(:name)
+  end
+end
---
+++ tic_tac_toe/ttt_spec.rb
@@ -0,0 +1,50 @@
+require_relative 'tic_tac_toe'
+
+describe Cell do
+
+  context "#initialize" do
+    it "is initialized with a value of '' by default" do
+      cell = Cell.new
+      expect(cell.value).to eq ''
+    end
+  end
+
+  it "can be initalized with a value of 'X' " do
+    cell = Cell.new("X")
+    expect(cell.value).to eq "X"
+  end
+
+end
+
+describe Player do
+
+  context "#initialize" do
+    it "raises an exception when initialized with {}" do
+      expect { Player.new({}) }.to raise_error
+    end
+
+    it "does not raise an error when initialized with a valid input" do
+      input = {color: "X", name: "someone"}
+      expect { Player.new(input) }.to_not raise_error
+    end
+  end #ends initialize context
+
+  context "#color" do
+    it "returns the color" do
+      input = { color: "X" , name: "Someone" }
+      player = Player.new(input)
+      expect(player.color).to eq "X"
+    end
+  end #end color context
+
+  context "#name" do
+    it "returns the player's name" do
+      input = { color: "X", name: "Spicoli" }
+      playa = Player.new(input)
+      expect(playa.name).to eq "Spicoli"
+    end
+  end
+
+
+
+end
