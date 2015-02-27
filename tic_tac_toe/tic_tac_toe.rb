d26cc7825b9bc09a0821b07f54d58ec88532cb1f
Alex Hardt <hardt.alex@gmail.com>
2015-02-27 08:48:36 -0600

    LT commit, debugging it

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -7,6 +7,10 @@ class Array
     self.all? {|element| element == self[0]}
   end
 
+  def not_all_same?
+    !all_same?
+  end
+
   def any_empty?
     self.any? { |element| element.to_s.empty? }
   end
@@ -39,7 +43,7 @@ class Board
   end
 
   def get_cell(x,y)
-    @grid[x][y]
+    grid[x][y]
   end
 
   def set_cell(x,y,value)
@@ -52,9 +56,15 @@ class Board
     false
   end
 
-  def draw?
-    grid.flatten.map { |cell| cell.value }.none_empty?
-  end #uses an array method i made, might need to include it or something
+
+  def formatted_grid
+    grid.each do |row|
+      puts row.map { |cell| cell.value.empty? ? "_" : cell.value }.join(" ")
+    end
+  end
+
+
+  private
 
   def winning_positions
     grid + # of rows
@@ -70,19 +80,30 @@ class Board
   end
 
   def winner?
+    #winning positions is a nested array with all of the possible combinations of winning line ups, if any one of the nested arrays has all 'x's or 'o's then the game is over and its a win for the player that owns that color
+    p "(84) winner? method"
     winning_positions.each do |winning_position|
-      next if winning_position_values(winning_position).all_empty?
+      p "(86) winner? method non culprit looks like: #{winning_position_values(winning_position)}"
+      next if winning_position_values(winning_position).not_all_same?
+      #this shouldn't be an 'all empty check, we want to just confirm that no one has won and move on to the next one'
+      p "(89) winner? method: pretty sure we're here in the winner"
+      p "(90)culprit?: #{winning_position_values(winning_position)}"
       return true if winning_position_values(winning_position.all_same?)
     end
     false
   end
 
   def winning_position_values(winning_position)
+    p " (97) method: winning position values being called, they are:"
     p "#{winning_position}"
+    p "#{winning_position.class}"
+    #so we are iterating over each element in the array, and that works, but we are running into a issue when we get to the end of the array
     winning_position.map { |cell| cell.value }
   end
 
-  private
+  def draw?
+    grid.flatten.map { |cell| cell.value }.none_empty?
+  end #uses an array method i made, might need to include it or something
 
   def default_grid
     Array.new(3) { Array.new(3) { Cell.new } }
@@ -113,6 +134,7 @@ class Game
   end
 
   def game_over_message
+    p "method: game over message (137)"
     return "#{current_player.name} won!" if board.game_over == :winner
     return "The game ended in a tie" if board.game_over == :draw
   end
@@ -120,14 +142,18 @@ class Game
   def play
     puts "#{current_player.name} has randomly been selected as the first player"
     while true
-      board
+      p "play method (145)"
+      board.formatted_grid
       puts ""
       puts solicit_move
       x, y = get_move
+      p "#{x} and #{y} "
       board.set_cell(x, y, current_player.color)
+      p "play method (152)"
       if board.game_over
+
         puts game_over_message
-        board
+        board.formatted_grid
         return
       else
         switch_players
@@ -140,13 +166,13 @@ class Game
   def human_move_to_coordinate(human_move)
     mapping = {
       "1" => [0, 0],
-      "2" => [1, 0],
-      "3" => [2, 0],
-      "4" => [0, 1],
+      "2" => [0, 1],
+      "3" => [0, 2],
+      "4" => [1, 0],
       "5" => [1, 1],
-      "6" => [2, 1],
-      "7" => [0, 2],
-      "8" => [1, 2],
+      "6" => [1, 2],
+      "7" => [2, 0],
+      "8" => [2, 1],
       "9" => [2, 2]
     }
     mapping[human_move]
